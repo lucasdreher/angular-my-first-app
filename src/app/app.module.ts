afe7782e1cc5b08f0822d2b604d08d3805372461
Lucas Dreher <lucasdreher@gmail.com>
2022-03-24 18:44:49 +0100

    importing FormsModule to enable ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [ AppComponent, ServerComponent, ServersComponent ],
-	imports: [ BrowserModule ],
+	imports: [ BrowserModule, FormsModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
